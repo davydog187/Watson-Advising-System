a53fea97004bc28c94c7dcfde395fdacb5824478
davydog187 <dlucia1@binghamton.edu>
2011-09-09 15:12:57 -0400

    Cleaned up the finished_student form

--- finish_student.php
+++ finish_student.php
@@ -2,8 +2,8 @@
     if(isset($_GET['rectime'])){
         $rectime = $_GET['rectime'];
         require_once('./php_core/connect_watson.php');
-        $query = "SELECT * FROM waiting w, students s WHERE s.bnumber=w.bnumber and w.rectime='$rectime'";  
-        
+        $query = "SELECT * FROM waiting w, students s WHERE s.bnumber=w.bnumber and w.rectime='$rectime'";
+
         $rows = mysql_query($query);
         $num_rows = mysql_num_rows($rows);
         if($num_rows==1){
@@ -25,26 +25,26 @@
 		<script src="js/jquery.js" type="text/javascript"></script>
 	</head>
 	<body>
-		<div id="wrapper"> 
+		<div id="wrapper">
 		  <?php require('header.html'); ?>
 			<div id="content">
 				<div id="finish_student">
-					<h2 class="center">Finish Student Walk-in</h2> 
+					<h2 class="center">Finish Student Walk-in</h2>
 					<form method="post" action="php_core/add_to_finished.php">
               <table>
                 <tr>
-							    <td>B-Number:</td><td><input type="text" value='<?php echo $student['bnumber']; ?>' disabled='disabled'></td>
+							    <td>B-Number:</td><td><input type="text" name="bnumber" value='<?php echo $student['bnumber']; ?>' disabled='disabled'></td>
                 </tr>
                 <tr>
-                  <td>Name:</td><td><?php echo $student['firstname']." ".$student['lastname']; ?></td>
+                  <td>Name:</td><td><input type="Text" name="name" value='<?php echo $student['firstname']." ".$student['lastname']; ?>' disabled='disabled'></td>
 						  <tr>
-                  <td>E-Mail:</td><td><?php echo $student['email']; ?></td>
+                  <td>E-Mail:</td><td><input type="text" name="email" value='<?php echo $student['email']; ?>' disabled='disabled'></td>
                 </tr>
                 <tr>
-							    <td>Program:</td><td><?php echo $student['major']; ?></td>
+							    <td>Program:</td><td><input type="text" name="major" value='<?php echo $student['major']; ?>' disabled='disabled'></td>
                 </tr>
                 <tr>
-                  <td>Reason for Visit:</td><td><?php echo $student['reason']; ?></td>
+                  <td>Reason for Visit:</td><td><input name="reason" type="text" value='<?php echo $student['reason']; ?>' disabled='disabled'></td>
                 </tr>
             </table>
             <br />
@@ -54,15 +54,15 @@
             Final Comments: <br />
             <textarea cols="40" rows="10" name="final_comments"></textarea> <br />
 						<input type="submit" value="Submit" />
-						
+
 					</form>
 
-				</div>		
+				</div>
 			</div>
-			
+
 			<div id="footer">
 				<p>Created by David Lucia and Nick Ciaravella</p>
 			</div>
 		</div>
-	</body>	
+	</body>
 </html>
