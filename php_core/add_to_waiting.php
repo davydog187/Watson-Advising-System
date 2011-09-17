2b845901fe269e320935cae0159bdc99bd657f5e
davydog187 <dlucia1@binghamton.edu>
2011-09-16 21:36:51 -0400

    fixed variable declarations

--- php_core/add_to_waiting.php
+++ php_core/add_to_waiting.php
@@ -2,23 +2,23 @@
 
 require_once("connect_watson.php");
 
-if(isset($_POST['bnumber']) && isset($_POST['initial_comments']) && isset($_POST['final_comments'])){
+if(isset($_POST['bnumber']) && isset($_POST['reason']) && isset($_POST['comments'])){
 	$reason = $_POST['reason'];
-	$bnumber = $_POST['bnumber']; //clean these
+	$bnumber = $_POST['bnumber'];
 	$comments = $_POST['comments'];
-}
-else {
 
-
-
-}
-
-$query = "INSERT INTO waiting (bnumber, reason, comments)";
-$query .= sprintf(" VALUES ('%s', '%s', '%s')", mysql_real_escape_string($bnumber),
+	$query = "INSERT INTO waiting (bnumber, reason, comments)";
+	$query .= sprintf(" VALUES ('%s', '%s', '%s')", mysql_real_escape_string($bnumber),
 		mysql_real_escape_string($reason), mysql_real_escape_string($comments));
 
+	mysql_query($query) or die("Error Inserting");
+	mysql_close($db);
 
-mysql_query($query) or die("Error Inserting");
+}
+else {
+	//error
 
-mysql_close($db);
+
+}
+
 ?>
